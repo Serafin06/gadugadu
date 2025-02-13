d0c36e5c99d5163ded92dcce4a43a78ad3b61e10
RafaL <zielony.banan88@gmail.com>
2025-02-13 16:33:36 +0100

    v1.01

--- RafaL/src/GaduGadu/ChatClient.java
+++ RafaL/src/GaduGadu/ChatClient.java
@@ -1,16 +1,13 @@
 package GaduGadu;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 public class ChatClient {
     String serverAddress;
@@ -19,6 +16,11 @@ public class ChatClient {
     JFrame frame = new JFrame("Gadu-GaduNew");
     JTextField textField = new JTextField(50);
     JTextArea messageArea = new JTextArea(16, 50);
+    private JList<String> userList;
+    private DefaultListModel<String> userListModel;
+
+
+
     public ChatClient(String serverAddress) {
         this.serverAddress = serverAddress;
         textField.setEditable(false);
@@ -26,23 +28,29 @@ public class ChatClient {
         frame.getContentPane().add(textField, BorderLayout.SOUTH);
         frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
         frame.pack();
-        // Send on enter then clear to prepare for next message
+        userListModel = new DefaultListModel<>();
+        userList = new JList<>(userListModel);
+        JScrollPane userScrollPane = new JScrollPane(userList);
+        frame.getContentPane().add(userScrollPane, BorderLayout.EAST);
+
         textField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                out.println(textField.getText());
+                String message = textField.getText();
+                out.println(message);
                 textField.setText("");
             }
         });
     }
+
     private String getName() {
         return JOptionPane.showInputDialog(
                 frame,
-                "Choose a screen name:",
-                "Screen name selection",
-                // tutaj zastrzerzenie!
+                "Wybierz swoje imie/pseudonim:",
+                "Wybor loginu",
                 JOptionPane.PLAIN_MESSAGE
         );
     }
+
     private void run() throws IOException {
         try {
             Socket socket = new Socket(serverAddress, 2222);
@@ -50,23 +58,36 @@ public class ChatClient {
             out = new PrintWriter(socket.getOutputStream(), true);
             while (in.hasNextLine()) {
                 String line = in.nextLine();
-                if (line.startsWith("BUSY")) {
+                if (line.startsWith("Serwer zajety, sproboj pozniej!")) {
                     messageArea.append(line.substring(5) + "\n");
-                }else if (line.startsWith("SUBMITNAME")) {
+                }else if (line.startsWith("BUSY")){
+                    JOptionPane.showMessageDialog(frame,"Ta nazwa jest zajeta. Wprowadz inna.","Blad", JOptionPane.ERROR_MESSAGE);
+
+                } else if (line.startsWith("SUBMITNAME")) {
                     out.println(getName());
-                    //alb tu sprawdzamy
                 } else if (line.startsWith("NAMEACCEPTED")) {
                     messageArea.append(line.substring(13) + "\n");
                     textField.setEditable(true);
+                } else if (line.startsWith("PRIVATE")) {
+                    messageArea.append("(PRIV) " + line.substring(8) + "\n");
                 } else if (line.startsWith("MESSAGE")) {
                     messageArea.append(line.substring(8) + "\n");
+                }else if (line.startsWith("USERLIST")){
+                    SwingUtilities.invokeLater(()->{
+                        userListModel.clear();
+                        String[] users = line.substring(9).split(" ");
+                        for(String u : users){
+                            if (!u.isEmpty()){
+                                userListModel.addElement(u);
+                            }
+                        }
+                    });
                 }
             }
         } finally {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
-// TODO Auto-generated catch block
                 e.printStackTrace();
             }
             in.close();
@@ -75,6 +96,7 @@ public class ChatClient {
             frame.dispose();
         }
     }
+
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println("Pass the server IP as the sole command line argument");
--- RafaL/src/GaduGadu/clientThread.java
+++ RafaL/src/GaduGadu/clientThread.java
@@ -1,4 +1,5 @@
 package GaduGadu;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,65 +11,116 @@ public class clientThread extends Thread {
     private PrintStream os = null;
     private Socket clientSocket = null;
     private final clientThread[] threads;
-    private int maxClientsCount;
+    private final int maxClientsCount;
+    private String name;
+
     public clientThread(Socket clientSocket, clientThread[] threads) {
         this.clientSocket = clientSocket;
         this.threads = threads;
         maxClientsCount = threads.length;
     }
-    public void run() {
-        int maxClientsCount = this.maxClientsCount;
-        clientThread[] threads = this.threads;
-        try {
-            /*
-             * Create input and output streams for this client.
-             */
 
-            is=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            os = new PrintStream(clientSocket.getOutputStream());
-            os.println("SUBMITNAME");
-
-            // albo tu prwdzamy imie
-            String name = is.readLine().trim();
-            os.println("NAMEACCEPTED Hello " + name + " to our chat room. To leave enter EXIT");
-            for (int i = 0; i < maxClientsCount; i++) {
-                if (threads[i] != null && threads[i] != this) {
-                    threads[i].os.println("MESSAGE *** A new user " + name + " entered the chat room!!! ***");
+    private void broadcastUserList () {
+        StringBuilder userList = new StringBuilder("USERLIST");
+        synchronized (threads) {
+            for (clientThread thread : threads) {
+                if (thread != null && thread.name != null) {
+                    userList.append(" ").append(thread.name);
                 }
             }
+            for (clientThread thread : threads) {
+                if (thread != null) {
+                    thread.os.println(userList.toString());
+                }
+            }
+        }
+    }
+
+    public void run() {
+        try {
+            is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            os = new PrintStream(clientSocket.getOutputStream());
+
+
+
+            synchronized (threads) {
+                do {
+                    os.println("SUBMITNAME");
+                    name = is.readLine().trim();
+                    boolean nameExists = false;
+
+                    for (clientThread t : threads) {
+                        if (t != null && t != this && t.name.equals(name)) {
+                            nameExists = true;
+                            break;
+                        }
+                    }
+                    if (!nameExists) {
+                        this.name = name;
+                        os.println("Akceptacja");
+                        break;
+                    } else {
+                        os.println("BUSY");
+                    }
+                } while (true);
+            }
+
+            os.println("NAMEACCEPTED Czesc " + name + " na naszym chacie. Aby wyjsc wpisz EXIT");
+            broadcastUserList();
+
+            for (int i = 0; i < maxClientsCount; i++) {
+                if (threads[i] != null && threads[i] != this) {
+                    threads[i].os.println("MESSAGE *** Nowy uzytkownik " + name + " podlaczyl sie na chat ***");
+                }
+            }
+
             while (true) {
                 String line = is.readLine();
-                if (line.startsWith("EXIT")) {
+                if (line.startsWith("EXIT") || line.equalsIgnoreCase("EXIT")) {
                     break;
                 }
-                if (line.startsWith("/<name>")){
-                    // tutaj na temat wyslania wiadomosci do uzytkownika
-                }
+                if (line.startsWith("/")) {
+                    String[] parts = line.split(" ", 2);
+                    if (parts.length > 1) {
+                        String recipient = parts[0].substring(1); //usuwanie "/"
+                        String message = parts[1];
 
-                for (int i = 0; i < maxClientsCount; i++) {
-                    if (threads[i] != null) {
-                        threads[i].os.println("MESSAGE <" + name + ">" + line);
+                        synchronized (threads) {
+                            for (clientThread t : threads) {
+                                if (t != null && t.name.equals(recipient)) {
+                                    t.os.println("PRIVATE " + name + ": " + message);
+                                    os.println("PRIVATE " + name + " -> " + recipient + ": " + message);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < maxClientsCount; i++) {
+                        if (threads[i] != null) {
+                            threads[i].os.println("MESSAGE <" + name + ">" + line);
+                        }
                     }
                 }
             }
             for (int i = 0; i < maxClientsCount; i++) {
                 if (threads[i] != null && threads[i] != this) {
-                    threads[i].os.println("MESSAGE *** The user " + name + " is leaving the chat room !!! ***");
+                    threads[i].os.println("MESSAGE *** Uzytkownik " + name + " opuscil chat ***");
                 }
             }
-            os.println("MESSAGE *** Bye " + name + " ***");
-            /*
-             * Clean up. Set the current thread variable to null so that a new client
-             * could be accepted by the server.
-             */
+            os.println("MESSAGE *** See " + name + " later. Pa :) ***");
+            this.os.close();
+            this.is.close();
+            this.clientSocket.close();
+
             for (int i = 0; i < maxClientsCount; i++) {
                 if (threads[i] == this) {
                     threads[i] = null;
+                    break;
                 }
             }
-            /*
-             * Close the output stream, close the input stream, close the socket.
-             */
+            broadcastUserList();
+
             is.close();
             os.close();
             clientSocket.close();
